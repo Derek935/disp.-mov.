7771a30589a97238274b653cf277ef5fb092a0b1
Derek935 <126626840+Derek935@users.noreply.github.com>
2024-06-03 16:18:12 -0600

    Add files via upload
    Esqueleto de la transicion de pantallas

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "juego2"
+include(":app")
